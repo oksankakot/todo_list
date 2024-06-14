5d4242df369019ef0de85026e71eba4d86a8e3c8
Oksana Kotlyarenko <kotlyuarenko.work@gmail.com>
2024-06-14 19:25:50 +0300

    Add permissions methods to User model

--- user/models.py
+++ user/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 
 class UserManager(BaseUserManager):
@@ -21,7 +21,7 @@ class UserManager(BaseUserManager):
         return self.create_user(username, password, **extra_fields)
 
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=255, null=False)
     last_name = models.CharField(max_length=255, null=True, blank=True)
     username = models.CharField(max_length=255, unique=True, null=False)
@@ -38,3 +38,8 @@ class User(AbstractBaseUser):
     def __str__(self):
         return self.username
 
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+
+    def has_module_perms(self, app_label):
+        return self.is_superuser
