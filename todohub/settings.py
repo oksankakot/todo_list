9a24110bb5705cfeb9dc95f5c6a6d81acc17ed06
Oksana Kotlyarenko <kotlyuarenko.work@gmail.com>
2024-06-16 22:18:27 +0300

    Quotes fixed

--- todohub/settings.py
+++ todohub/settings.py
@@ -113,13 +113,13 @@ WSGI_APPLICATION = "todohub.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'todolist',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "todolist",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
