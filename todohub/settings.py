60c654720f4698700eedb126173f4868f490572f
Oksana Kotlyarenko <kotlyuarenko.work@gmail.com>
2024-06-16 19:11:10 +0300

    Use PostgreSQL as the database to store data. Create the corresponding database configuration and deploy PostgreSQL using Docker.

--- todohub/settings.py
+++ todohub/settings.py
@@ -113,11 +113,16 @@ WSGI_APPLICATION = "todohub.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todolist',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 AUTH_USER_MODEL = "user.User"
 
