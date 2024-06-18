0ba70dd9ca106e11b3125c60790b5e0218e76be7
Oksana Kotlyarenko <kotlyuarenko.work@gmail.com>
2024-06-18 12:46:01 +0300

    Add endpoints for getting list of all tasks and marking task as complete

--- tasktracker/views.py
+++ tasktracker/views.py
@@ -1,5 +1,8 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import Task
 from .serializers import TaskSerializer
 
@@ -35,6 +38,12 @@ class TaskRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
             return Task.objects.all()
         return Task.objects.filter(user=self.request.user)
 
+    def patch(self, request, *args, **kwargs):
+        task = self.get_object()
+        task.status = "completed"
+        task.save()
+        return Response(self.get_serializer(task).data)
+
 
 class TaskListByStatusView(generics.ListAPIView):
     serializer_class = TaskSerializer
@@ -46,3 +55,24 @@ class TaskListByStatusView(generics.ListAPIView):
         if self.request.user.is_superuser:
             return Task.objects.filter(status=task_status)
         return Task.objects.filter(status=task_status, user=self.request.user)
+
+
+class AllTasksListView(generics.ListAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    pagination_class = CustomPagination
+
+
+class MarkTaskAsCompletedView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def patch(self, request, pk, format=None):
+        try:
+            task = Task.objects.get(pk=pk, user=request.user)
+        except Task.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        task.status = 'completed'
+        task.save()
+        serializer = TaskSerializer(task)
+        return Response(serializer.data, status=status.HTTP_200_OK)
